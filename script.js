9c7491b3d589daf9ab2ba5e8ff6aab8cc1bd0b83
iamgame3 <kosmosworld9@hotmail.com>
2022-12-24 11:44:36 -0500

    Add error messages when passwords do not match

---
+++ script.js
@@ -0,0 +1,15 @@
+addEventListener('click', () => {
+    const pass = document.getElementById('pass');
+    const conpass = document.getElementById('conpass');
+    const passMatchError = document.querySelector('.pass-error')
+
+    if (pass.value !== conpass.value) {
+        passMatchError.textContent = 'Passwords do not match *';
+        pass.setCustomValidity('Passwords must match.');
+        conpass.setCustomValidity('Passwords must match.');
+    } else {
+        passMatchError.textContent = '';
+        pass.setCustomValidity('');
+        conpass.setCustomValidity('');
+    }
+})
